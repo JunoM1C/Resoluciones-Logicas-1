6fb6136f76265b1224da22526dff42c5a760d516
Juno <gastonmicarelli441@gmail.com>
2025-11-18 19:46:15 -0300

    Add files via upload

---
+++ Trabajo_PrácticoN°1/index.js
@@ -0,0 +1,13 @@
+//variables en JS
+
+let provincia = "Mendoza"
+
+var nombre = "Gastón"
+
+//el punto y coma es opcional en JS
+const DNI = 49402368;
+
+//imprimimos  la variable en consola 
+console.log(nombre);
+console.log(provincia);
+console.log(DNI);
